f4b3336a70c89c032d5509b49ccf1f992de55630
Dilip Kumar <dilipkumarconnect@gmail.com>
2025-10-23 23:16:26 +0530

    Time: 6 ms (9.28%) | Memory: 12.5 MB (52.62%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,8 +1,10 @@
 class Solution(object):
     def sortColors(self, nums):
+        triger = False
         for i in range(len(nums)):
             for j in range(0,len(nums)-i-1):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
-        return nums
-        
+                    triger = True
+            if not triger:
+                break
