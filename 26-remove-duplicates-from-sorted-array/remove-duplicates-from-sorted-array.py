f6bf0687b2b72721d1dd53f83e0ed61524f3baf0
Dilip Kumar <dilipkumarconnect@gmail.com>
2025-12-03 15:14:48 +0530

    Time: 33 ms (12.41%) | Memory: 14 MB (38.80%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,8 +1,10 @@
-class Solution:
+class Solution(object):
     def removeDuplicates(self, nums):
-        temp = []
+        flist = []
         for i in range(len(nums)):
-            if nums[i] not in temp:
-                temp.append(nums[i])
-        nums[:]=temp
+            if nums[i] not in flist:
+                flist.append(nums[i])
+                
+        nums[:]=flist
         return len(nums)
+        
