9ae94e4347af1bb5a6d261203b9b3554460312f1
Dilip Kumar <dilipkumarconnect@gmail.com>
2026-01-08 21:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (65.37%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,17 +1,15 @@
-class Solution(object):
+class Solution:
     def search(self, nums, target):
-        n = len(nums)
         low=0
-        high = n-1
+        high=len(nums)-1
         
-        while low<= high:
+        while low<=high:
             mid = (low+high)//2
             if nums[mid]==target:
                 return mid
-            elif nums[mid] <target:
+            elif target > nums[mid]:
                 low = mid+1
             else:
                 high = mid-1
         return -1
-        
         
