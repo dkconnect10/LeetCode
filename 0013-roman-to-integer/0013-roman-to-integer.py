a5956c27ff6c7d2b419e5cf11d99eeb704866dd9
Dilip Kumar <dilipkumarconnect@gmail.com>
2026-02-04 19:56:36 +0530

    Time: 5 ms (52.28%), Space: 19.4 MB (31.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,56 +1,12 @@
 class Solution:
-
     def romanToInt(self, s: str) -> int:
-
-        temp=[]
-
-        temp1=[]
-
-        for i in s:
-
-            if i=="I":temp.append(1)
-
-            if i=="V":temp.append(5)
-
-            if i=="X":temp.append(10)
-
-            if i=="L":temp.append(50)
-
-            if i=="C":temp.append(100)
-
-            if i=="D":temp.append(500)
-
-            if i=="M":temp.append(1000)
-
-        if len(temp)==1:
-
-            return temp[0]
-
-        if len(temp)==0:
-
-            return ""
-
-        j=len(temp)-1
-
-        while j>0:
-
-            if temp[j-1]<temp[j]:
-
-                temp1.append(temp[j]-temp[j-1])
-
-                j-=1
-
-            else:    
-
-                temp1.append(temp[j])
-
-            j-=1
-
-        print(temp)    
-
-        if temp[0]>=temp[1]:
-
-            temp1.append(temp[0])
-
-        return sum(temp1)
- 
+        roman ={
+            "I":1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000
+        } 
+        total=0
+        for i in range(len(s)):
+            if i+1<len(s) and roman[s[i]]<roman[s[i+1]]:
+                total-=roman[s[i]]
+            else:
+                total+=roman[s[i]]
+        return total
