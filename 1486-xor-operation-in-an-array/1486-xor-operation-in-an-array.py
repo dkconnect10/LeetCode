09fa386790295e091277bde2b3aa5054073ed744
Dilip Kumar <dilipkumarconnect@gmail.com>
2026-05-13 22:21:14 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.11%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -1,10 +1,6 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
-        temp = []
-        for i in range(n):
-            temp.append(start+2*i)
-        
         freq=0
-        for val in temp:
-            freq^=val
+        for i in range(n):
+            freq^=start+2*i
         return freq 
