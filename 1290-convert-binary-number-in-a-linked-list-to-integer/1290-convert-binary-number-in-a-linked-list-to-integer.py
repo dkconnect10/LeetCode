cae8c011059297d3f276a8d12ba127f51ba1ab17
Dilip Kumar <dilipkumarconnect@gmail.com>
2026-05-19 08:23:43 +0530

    Time: 0 ms (100%), Space: 19.3 MB (15.62%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -3,28 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def reverseLinkLIst(self,head):
-        temp=head
-        prev = None
-        while temp:
-            front = temp.next
-            temp.next=prev
-            prev=temp
-            temp=front
-        head=prev
-        return head
-
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
-        head = self.reverseLinkLIst(head)
-        temp = head
-        num = 0
-        p = 1
+        temp=head
+        num=0
         while temp:
-            if temp.val==1:
-                num+=p
-            p=p*2
+            num = num*2+temp.val
             temp=temp.next
-        return num        
-    
+        return num    
+        
